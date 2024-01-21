bb39f3beb3937320cadf2318f190d08160331376
Joanavel Pascual <joanavelpascual@pursuit.org>
2024-01-21 18:59:45 -0500

    updated homepage

--- src/pages/New.js
+++ src/pages/New.js
@@ -3,7 +3,7 @@ import EventNewForm from "../components/EventNewForm";
 
 function New() {
   return (
-    <div className="container"> 
+    <div id="new-event-container"> 
       {/* <h1> New </h1> */}
       <EventNewForm/>
     </div>
