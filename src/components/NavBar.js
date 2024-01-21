d7a801c32455a9f0abbdd63a4fd41abb440cf379
Joanavel Pascual <joanavelpascual@pursuit.org>
2024-01-21 01:41:47 -0500

    updated app logo and name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './NavBar.css'
+import './NavBar.css';
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/users">Users</Link> 
+      <Link to="/" id="logo">
+        Home
+      </Link>
+      <Link to="/users">Users</Link>
     </nav>
   );
 }
