3dd1b6fd7009f2da708ac7041ac868139660f56e
Joanavel Pascual <joanavelpascual@pursuit.org>
2024-01-28 21:44:19 -0500

    fixed modal, now need to work on placement

--- src/components/EventNewForm.js
+++ src/components/EventNewForm.js
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import '../CSS/EventNewForm.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "../CSS/EventNewForm.css";
+
+import logo from "../Images/Reviews.png";
 
 export default function EventNewForm({ openModal, closeModal }) {
   const initialFormData = {
-    name: '',
-    date: '',
-    location: '',
-    rating: '',
-    comment: '',
+    name: "",
+    date: "",
+    location: "",
+    rating: "",
+    comment: "",
     is_favorite: false,
-    user_name: '',
+    user_name: "",
   };
   const [formData, setFormData] = useState(initialFormData);
 
   const handleInputChange = (event) => {
     const { name, value, type, checked } = event.target;
-    const inputValue = type === 'checkbox' ? checked : value;
+    const inputValue = type === "checkbox" ? checked : value;
 
     setFormData({
       ...formData,
@@ -30,92 +32,123 @@ export default function EventNewForm({ openModal, closeModal }) {
     axios
       .post(`${process.env.REACT_APP_API_URL}/events`, formData)
       .then((response) => {
-        console.log('New Event added:', response.data);
+        console.log("New Event added:", response.data);
         setFormData(initialFormData);
         closeModal();
       })
       .catch((error) => {
-        console.error('API Error:', error);
+        console.error("API Error:", error);
       });
   };
 
   return (
-    <div className={`modal-overlay ${openModal ? 'modal-open' : ''}`}>
-      <div className="modal-content">
-        <span className="close" onClick={closeModal}>
+    <div id={`modal-overlay ${openModal ? "modal-open" : ""}`}>
+      <div id="modal-content">
+          <div id="close-container">
+        <span id="close" onClick={closeModal}>
           &times;
         </span>
-        <div className="EventNewForm">
-          <h2>Add New Event</h2>
-          <form onSubmit={handleSubmit}></form>
-      <form onSubmit={handleSubmit}>
-        <label className='name'>Name:</label>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
-        <label className='date'>Date:</label>
-        <input
-          type="date"
-          name="date"
-          value={formData.date}
-          onChange={handleInputChange}
-        />
-        <label className='location'>Location:</label>
-        <input
-          type="text"
-          name="location"
-          value={formData.location}
-          onChange={handleInputChange}
-        />
-        <label className='rating'>Rating:</label>
-        <input
-          type="number"
-          name="rating"
-          placeholder="Raiting"
-          value={formData.rating}
-          onChange={handleInputChange}
-        />
-        <label className='comment'>Comment:</label>
-        <textarea
-          name="comment"
-          placeholder="Comment"
-          value={formData.comment}
-          onChange={handleInputChange}
-        />
-        <div>
-          <input
-            type="checkbox"
-            id="is_favorite"
-            name="is_favorite"
-            checked={formData.is_favorite}
-            onChange={handleInputChange}
-          />
-          <label className='favorite' htmlFor="is_favorite">Favorite</label>
         </div>
-        <label className='user-name'>User Name:</label>
-        <input
-          type="text"
-          name="user_name"
-          value={formData.user_name}
-          onChange={handleInputChange}
-        />
-        <div>
-        <label className='image'>Image URL:</label>
-        <input
-          type="text"
-          name="image_url"
-          value={formData.image_url}
-          onChange={handleInputChange}
-        />
-            <button className="add-event-button" type="submit">
-              Add Event
-            </button>
+        <div id="add-modal-conent">
+        <div className="EventNewForm">
+            <h2 id="add-h2">Add New Event</h2>
+       
+          <form onSubmit={handleSubmit}>
+            <label id="add-name" className="name"></label>
+            <input
+              type="text"
+              placeholder="Name"
+              id="add-name-input"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+            <label id="add-date" className="date"></label>
+            <input
+              type="date"
+              id="add-date-input"
+              name="date"
+              value={formData.date}
+              onChange={handleInputChange}
+            />
+            <label id="add-location" className="location"></label>
+            <input
+              type="text"
+              id="add-location-input"
+              name="location"
+              placeholder="Location"
+              value={formData.location}
+              onChange={handleInputChange}
+            />
+            <label id="add-rating" className="rating"></label>
+            <input
+              type="number"
+              id="add-rating-input"
+              name="rating"
+              placeholder="Rating"
+              value={formData.rating}
+              onChange={handleInputChange}
+            />
+            <label id="add-comment" className="comment"></label>
+            <textarea
+              id="add-comment-input"
+              name="comment"
+              placeholder="Comment"
+              value={formData.comment}
+              onChange={handleInputChange}
+            />
+
+            <label id="add-username" className="user-name"></label>
+            <input
+              type="text"
+              placeholder="User Name"
+              id="add-username-input"
+              name="user_name"
+              value={formData.user_name}
+              onChange={handleInputChange}
+            />
+            <div>
+              <label id="add-image" className="image">
+              </label>
+              <input
+                type="text"
+                placeholder="Image URL"
+                id="add-image-input"
+                name="image_url"
+                value={formData.image_url}
+                onChange={handleInputChange}
+              />
+
+              <div>
+                <input
+                  type="checkbox"
+                  id="is_favorite"
+                  name="is_favorite"
+                  placeholder="User Name"
+                  checked={formData.is_favorite}
+                  onChange={handleInputChange}
+                />
+                <label
+                  id="add-favorite-label"
+                  className="favorite"
+                  htmlFor="is_favorite"
+                >
+                  Favorite
+                </label>
+              </div>
+              <button
+                id="add-button"
+                className="add-event-button"
+                type="submit"
+              >
+                Add Event
+              </button>
             </div>
           </form>
         </div>
+
+        </div>
+
       </div>
     </div>
   );
